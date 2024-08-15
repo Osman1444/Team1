c17150b5092780ae92c2019f8c77ad83c952eb3c
Osman1444 <130256195+Osman1444@users.noreply.github.com>
2024-08-15 10:25:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,30 +8,40 @@ function showSection(sectionId) {
     const timer = document.getElementById('timer-content');
     const calendar = document.getElementById('calendar-content');
     const task = document.getElementById('task-content');
+    const feedback = document.getElementById('feedback-content');
 
     if (sectionId == 'home'){
         home.style.transform = 'scale(1)';
         timer.style.transform = 'scale(0)';
         calendar.style.transform = 'scale(0)';
         task.style.transform = 'scale(0)';
+        feedback.style.transform = 'scale(0)';
 
     } else if (sectionId == 'timer'){
         home.style.transform = 'scale(0)';
         timer.style.transform = 'scale(1)';
         calendar.style.transform = 'scale(0)';
         task.style.transform = 'scale(0)';
+        feedback.style.transform = 'scale(0)';
 
     } else  if (sectionId == 'calendar'){
         home.style.transform = 'scale(0)';
         timer.style.transform = 'scale(0)';
         calendar.style.transform = 'scale(1)';
         task.style.transform = 'scale(0)';
+        feedback.style.transform = 'scale(0)';
 
     } else if (sectionId == 'task'){
         home.style.transform = 'scale(0)';
         timer.style.transform = 'scale(0)';
         calendar.style.transform = 'scale(0)';
         task.style.transform = 'scale(1)';
-
+        feedback.style.transform = 'scale(0)';
+    } else if (sectionId == 'feedback'){
+        home.style.transform = 'scale(0)';
+        timer.style.transform = 'scale(0)';
+        calendar.style.transform = 'scale(0)';
+        task.style.transform = 'scale(0)';
+        feedback.style.transform = 'scale(1)';
     }
 }
